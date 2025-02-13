cd373df1e0a3aa7dbb5adc62a0d9baede248bfe5
Nnamdi Michael Okpala <nnamdi@obinexuscomputing.org>
2025-02-13 15:30:58 +0000

    w

---
+++ nexus-search/dist/types/src/types/mapper.d.ts
@@ -0,0 +1,14 @@
+import { IndexConfig, IndexedDocument } from ".";
+export interface MapperState {
+    trie: unknown;
+    dataMap: Record<string, string[]>;
+    documents: Array<{
+        key: string;
+        value: IndexedDocument;
+    }>;
+    config: IndexConfig;
+}
+export interface MapperOptions {
+    caseSensitive?: boolean;
+    normalization?: boolean;
+}
---
+++ nexus-search/dist/types/src/utils/ValidationUtils.d.ts
@@ -0,0 +1,4 @@
+import { SearchOptions, IndexConfig, SearchableDocument } from "@/types";
+export declare function validateSearchOptions(options: SearchOptions): void;
+export declare function validateIndexConfig(config: IndexConfig): void;
+export declare function validateDocument(document: SearchableDocument, fields: string[]): boolean;
